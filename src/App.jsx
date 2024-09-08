d84103b8fa4506c81b07b0c60717266dc8dd42ef
KaueDeMoura <kaueoenningdmoura@gmail.com>
2024-09-07 22:36:06 -0300

    Feat: Login e Registrar com token

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Login/register';
+import Home from './pages/Home';
 import Pokedex from './pages/poke';
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pokedex" element={<Pokedex />} />
+  <Route path="/login" element={<Login/>} />
+  <Route path="/registrar" element={<Register/>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/pokedex" element={<Pokedex/>} />
     </Routes>
   );
 }
